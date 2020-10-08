f84e9bbb145cd4d798caf4b76fa510fe51b878d3
Evgeny Samsonov <samsonov@aeroidea.ru>
2020-10-08 16:10:00 +0300

    added src files for static

---
+++ static/src/js/main.js
@@ -0,0 +1,43 @@
+// Wait submit event to input
+// Returns nothing
+function formConsole(event) {
+  event.preventDefault();
+  var formData = new FormData(event.target);
+  var result = {};
+
+  for (var pair of formData.entries()) {
+    result[pair[0]] = pair[1];
+  }
+  console.log(result);
+}
+
+// Wait for input selector to input ( string - .js-*)
+// Add 'is-value' class if input has value in it
+// Returns nothing
+function fieldsIsValueClassAdding(selector) {
+  const fields = document.querySelectorAll(selector);
+
+  fields.forEach((field) => {
+    const inputField = field.querySelector('input');
+    addIsValueClassToFields(inputField);
+
+    inputField.addEventListener('blur', function (event) {
+      if (event && event.target) addIsValueClassToFields(event.target);
+    });
+  });
+
+  // Wait for node to input
+  // toggle class whether input has value or not
+  // Returns nothing
+  function addIsValueClassToFields(inputField) {
+    if (inputField.value.length) {
+      inputField.classList.add('is-value');
+    } else {
+      inputField.classList.remove('is-value');
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  fieldsIsValueClassAdding('.js-field');
+});
