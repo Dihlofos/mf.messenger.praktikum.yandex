fa38503f7363c8f7fc5ab0375a8dff9359bb04b7
Evgeny Samsonov <samsonov@aeroidea.ru>
2020-11-13 18:28:05 +0300

    added lost vendor.js

--- build/assets/js/components/CharCreateModal/CharCreateModal.d.ts
+++
@@ -1,9 +0,0 @@
-import { Block } from '../../modules/Block.js';
-export declare type ChatProps = {
-    mix?: string;
-};
-export declare class ChatCreaModal extends Block {
-    messages: string[];
-    constructor(props: ChatProps);
-    render(): any;
-}
--- build/assets/js/utils/formConsole.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function formConsole(form: HTMLFormElement): {
-    [key: string]: FormDataEntryValue;
-} | undefined;
